7d1851516b8bd128220c4c957b3200be2ed9bb08
Xholi <90756178+Xholi@users.noreply.github.com>
2024-06-20 18:59:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def main():
         try:
             df = load_data(uploaded_file)
             st.sidebar.success('File successfully uploaded!')
-        else:
+    else:
             df = pd.read_csv("Titanic_DAta.csv")
             st.sidebar.success(' successfully uploaded!')
         except Exception as e:
